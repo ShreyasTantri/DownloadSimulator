b1c310a8511594181d0ae24f98021a788dba73c1
ShreyasTantri <shreyastantrik29@gmail.com>
2026-09-07 20:11:54 +0530

    Initial Commit

---
+++ DownloadManagerSim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DownloadManagerSim
+//
+//  Created by CCS038 on 07/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DownloadManagerSim/DownloadManagerSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadManagerSimApp.swift
+//  DownloadManagerSim
+//
+//  Created by CCS038 on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct DownloadManagerSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
